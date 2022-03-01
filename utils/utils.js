d1fd27ea2b0853752c7a4817cb06fbbc12f1551f
younesftaich <76970345+younesftaich@users.noreply.github.com>
2022-03-02 01:44:36 +0800

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
 export function gettoken() {
-    return "AbUWTEtB39uPdHT0jqF9nmBsFkZ63JU3xbRxu8_yjOE6K4ISe90o2qDType4X07c70rxUx972LMfnudl"
+    return "AV8a9nTt7ONd9sIDOZ16ltTd_INOyr6FsOfRPCLNjY_-NsDXd8FrgBWHTrjzBr6vuol6BrBKHbaaHRra"
   }
  
